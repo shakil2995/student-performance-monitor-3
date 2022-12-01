d3eec9040990815cc44d1cc1524eeab53e9d0022
Shakil <shakil2995@gmail.com>
2022-12-01 22:05:19 +0600

    stable

--- pages/subpages/addquestion.js
+++ pages/subpages/addquestion.js
@@ -8,23 +8,27 @@ const Addquestion = ({ children }) => {
             semester: '',
             section: '',
             question: [],
+            submit: 'Submit',
         },
         onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+            formik.values.submit = 'Submitted'
+            console.log(values);
         },
 
     })
     return (
-        <div className={styles.formContainer}>
+        <form onSubmit={formik.handleSubmit} className={styles.formContainer}>
             {/* create a form from formik data */}
             <div className={styles.courseField}>
-                Course ID :<input className={styles.formItem} type="text" name="course_id" onChange={formik.handleChange} value={formik.values.course_id} />
-                Semester :<input className={styles.formItem} type="text" name="semester" onChange={formik.handleChange} value={formik.values.semester} />
-                Section :<input className={styles.formItem} type="text" name="section" onChange={formik.handleChange} value={formik.values.section} />
-                Question :<input className={styles.formItem} type="text" name="question" onChange={formik.handleChange} value={formik.values.question} />
+                Course ID :<input className={styles.formItem} placeholder="Course ID" type="text" required name="course_id" onChange={formik.handleChange} value={formik.values.course_id} />
+                Semester :<input className={styles.formItem} placeholder="Semester" type="text" required name="semester" onChange={formik.handleChange} value={formik.values.semester} />
+                Section :<input className={styles.formItem} placeholder="Section" type="text" required name="section" onChange={formik.handleChange} value={formik.values.section} />
+                Question :<input className={styles.formItem} placeholder="Question" type="text" required name="question" onChange={formik.handleChange} value={formik.values.question} />
+
+
             </div>
-            <button className={styles.formButton} type="submit">Submit</button>
-        </div>
+            <button className={styles.formButton} type="submit">{formik.values.submit}</button>
+        </form>
     );
 
 }
